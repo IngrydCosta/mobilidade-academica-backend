ce8c2019811f8d14f3aea7e6f115c4aca04a9d7c
Ingryd Costa <70590036+IngrydCosta@users.noreply.github.com>
2026-06-08 21:16:27 -0300

    instalando cors na aplicação

--- src/App.ts
+++ src/App.ts
@@ -3,11 +3,12 @@ import routes from "./routes/index"
 import userRoutes from "./routes/userRoutes";
 
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use(routes);
-
-app.use("/users", userRoutes);
 
 export { app };
