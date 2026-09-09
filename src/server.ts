310cdea0f397889e2e52e20b181e4f0fad0d8273
Ingryd Costa <70590036+IngrydCosta@users.noreply.github.com>
2026-09-08 21:44:45 -0300

    ajustes finais 2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import { app } from "./App"
+import "dotenv/config";
+import { app } from "./App";
 
-app.listen(3333, () => {
-  console.log("Servidor iniciado");
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
 });
